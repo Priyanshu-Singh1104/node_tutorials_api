fec78b2c8420f187e1cf5dee8a9af9aa046d27f7
Priyanshu <hppriyanshu1104@gmail.com>
2022-01-02 01:29:02 +0530

    add file system

---
+++ data.js
@@ -0,0 +1,7 @@
+// const data = { name: 'Priyanshu Singh', email: 'hppriyanshu1104@gmail.com' }  // Single user data
+const data = [
+        { name: 'Priyanshu Singh', email: 'hppriyanshu1104@gmail.com' },
+        { name: 'Happy Singh', email: 'happy@gmail.com' },
+        { name: 'Ajay Chauhan', email: 'ajay@gmail.com' },
+    ] //Multi User Data
+module.exports = data;
--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const colors = require('colors');
-console.log("hello".red);
+const http = require('http');
+const data = require('./data');
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application\json' });
+    res.write(JSON.stringify(data));
+    res.end();
+}).listen(4000);
 
 
+//create / remove a file usingcomandand pass input parameter using command
+// const fs = require('fs');
+// const input = process.argv;
+// if (input[2] == 'add') {
+//     fs.writeFileSync(input[3], input[4]);
+// } else if (input[2] == 'remove') {
+//     fs.unlinkSync(input[3]);
+// } else {
+//     console.log('Invalid Output');
+// }
 
 
+//status 200: ok
+// status 201:created
+// status 404:Not Found
+//Status: 500 Internal Server Error
+// console.log(process);
+// console.log(process.argv);
 
-
-
+// const colors = require('colors');
+// console.log("hello".blue);
 
 // const http = require('http');
 
