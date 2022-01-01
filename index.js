56107dbd07d2c3c6bc814ed1f6f23276761c529d
Priyanshu <hppriyanshu1104@gmail.com>
2022-01-01 22:00:25 +0530

    add gitignore

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const http = require('http');
+const colors = require('colors');
+console.log("hello".red);
 
-function dataControl(req, res) {
-    res.write('<h1>Hello this is Priyanshu Singh</h1>');
-    res.end();
-}
 
-http.createServer(dataControl).listen(4500);
+
+
+
+
+
+
+// const http = require('http');
+
+// function dataControl(req, res) {
+//     res.write('<h1>Hello this is Priyanshu Singh</h1>');
+//     res.end();
+// }
+
+// http.createServer(dataControl).listen(4500);
 
 // http.createServer((req, res) => {
 //     res.write('<h1>Hello this is Priyanshu Singh</h1>');
