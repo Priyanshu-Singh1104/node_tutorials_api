387cdd36d9d221ba1eebd388d54724c8187fd05c
Priyanshu <hppriyanshu1104@gmail.com>
2022-01-02 18:45:11 +0530

    add dynamic ejs  Website

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 
 const app = express();
+const reqFilter = (req, res, next) => {
+    if (!req.query.age) {
+        res.send("Please Provide Your gae")
+    } else if (req.query.age < 18) {
+        res.send("You can't access this Page")
+    } else {
+        next();
+    }
 
+}
+
+app.use(reqFilter);
+app.get('/', (req, res) => {
+    res.send('Welcome to Home Page');
+});
+
+app.get('/users', (req, res) => {
+    res.send('Welcome to Users Page');
+});
 
 app.listen(2000);
